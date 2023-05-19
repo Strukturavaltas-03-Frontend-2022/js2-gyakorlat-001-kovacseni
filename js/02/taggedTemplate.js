e8a8e6508de47c3c692b1cd45ef3edba1b60dd7d
kovacseni <kovacseni@gmail.com>
2023-05-19 06:47:41 +0200

    javítás

--- js/02/taggedTemplate.js
+++ js/02/taggedTemplate.js
@@ -1,6 +1,7 @@
-export default taggedTemplate;
 
 const taggedTemplate = (texts, ...values) =>
     texts.map((text, index) =>
         `${`<em>${text}</em>`}${values[index] ? `<strong>${values[index]}</strong>` : ''}`)
         .join('');
+
+export default taggedTemplate;
