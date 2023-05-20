e95300e1f796ad899cf8c9041b1528ca57ac80ac
kovacseni <kovacseni@gmail.com>
2023-05-20 10:51:14 +0200

    ez így?

--- js/02/taggedTemplate.js
+++ js/02/taggedTemplate.js
@@ -1,7 +1,8 @@
 
 const taggedTemplate = (texts, ...values) =>
     texts.map((text, index) =>
-        `${`<em>${text}</em> `}${values[index] ? `<strong>${values[index]}</strong> ` : ''}`)
+        `${text != ' ' ? `<em>${text}</em>` : ' '}
+        ${values[index] ? `<strong>${values[index]}</strong> ` : ''}`)
         .join('');
 
 export default taggedTemplate;
